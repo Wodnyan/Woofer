660084823f22790cea66599a4d42439551c7f115
Wodnyan <gergoalbert.gta@gmail.com>
2020-04-16 22:15:44 +0300

    Connected to database and created model/schema for users

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,11 @@
+const {User} = require("../database/database.js");
+new User({username: "foobar", password: "barFoo"}).save((err)=>{
+  if(err) console.error(err);
+  console.log("Saved User");
+})
+const {hash, compare} = require("bcrypt");
+const saltRounds = 10;
+const plainPassword = "Foobar";
 module.exports = (app)=>{
   app.post("/user/login",(req,res)=>{
       console.log(req.body);
---
+++ backend/database/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://user:user@my-cluster-jv3bc.mongodb.net/users?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology: true},(err)=>{
+  if(err) console.error(err);
+  console.log("Connected to Database");
+})
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  username: String,
+  password: String
+})
+const User = mongoose.model("Foobar", UserSchema);
+module.exports = {
+  User
+}
