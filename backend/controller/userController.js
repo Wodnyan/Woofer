ef7e444925e1d1c1d24d44fc33713323e60f3476
Wodnyan <gergoalbert.gta@gmail.com>
2020-04-17 21:17:38 +0300

    Users can now login and register

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,19 +1,48 @@
 const {User} = require("../database/database.js");
-new User({username: "foobar", password: "barFoo"}).save((err)=>{
-  if(err) console.error(err);
-  console.log("Saved User");
-})
 const {hash, compare} = require("bcrypt");
 const saltRounds = 10;
-const plainPassword = "Foobar";
+//Middleware to check for duplicate usernames
+function checkUserName(req, res, next){
+  const {username} = req.body;
+  User.findOne({username: username }, (err, foo)=>{
+    if(err) console.error(err);
+    else if(foo === null) next();
+    else{
+      console.log("Username Taken ");
+      res.send({
+        error: "Username Taken"
+      })
+      return;
+    }
+  })
+}
 module.exports = (app)=>{
-  app.post("/user/login",(req,res)=>{
-      console.log(req.body);
-      res.json(req.body);
+  //LOGIN
+  app.post("/user/login", (req,res)=>{
+      const {username, password} = req.body;
+      User.findOne({username: username}, async (err, data)=>{
+        if(err) console.log("Foo");
+        else if(data){
+          const passIsCorrect = await compare(password, data.password);
+          res.json(req.body);
+        }
+        else{
+          console.log("No such username");
+          res.json({error: "No such username"})
+        }
+      })
   })
-  app.post("/user/signup",(req,res)=>{
-    console.log(req.body);
-    res.json(req.body)
+  //REGISTER
+  app.post("/user/signup", checkUserName, async (req,res)=>{
+    const userInfo = req.body;
+    const hashedPw = await hash(userInfo.password, saltRounds);
+    new User({username: userInfo.username, password: hashedPw}).save((err)=>{
+      if(err) console.error(err);
+      console.log(`${userInfo.username} created an account`);
+    })
+    res.status(200).send(req.body);
   })
-  // TODO: Create an api
+  app.get('/api/woofers',(req, res)=>{
+    res.send("Woofer Api")
+  })
 };
