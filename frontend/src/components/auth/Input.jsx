2b590e40d39cec8dcdc75d642824d71c036aa496
Wodnyan <gergoalbert.gta@gmail.com>
2020-04-18 14:19:09 +0300

    Handling errors on the users end

--- frontend/src/components/auth/Input.jsx
+++ frontend/src/components/auth/Input.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+class User{
+  constructor(un, pw){
+    this.username = un;
+    this.password = pw;
+  }
+}
 export default class Input extends React.Component{
   constructor(props){
     super(props)
@@ -19,15 +24,25 @@ export default class Input extends React.Component{
   }
   async handleSubmit(e, url){
     e.preventDefault();
+    const {username, password} = this.state;
+    if(!username || !password){
+      this.setState({
+        error: "Enter username and password"
+      })
+      return;
+    }
+    const user = new User(username, password)
     const post = await fetch(url,{
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(this.state)
+      body: JSON.stringify(user)
     })
     const resp = await post.json();
-    console.log(resp);
+    this.setState({
+      error: resp.error
+    })
   }
   render(){
     const {title, url} = this.props
@@ -37,6 +52,7 @@ export default class Input extends React.Component{
       {title}
     </h1>
       <form onSubmit={(e)=>{this.handleSubmit(e, url)}}>
+        <h1 style={{color: "red"}}>{this.state.error}</h1>
         <input type="text" name="username" onChange={this.handleInputChange} value={this.state.username}></input>
         <input type="password" name="password" onChange={this.handleInputChange} value={this.state.password}></input>
         <button type="submit">{title}</button>
