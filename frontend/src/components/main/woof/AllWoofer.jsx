6a92dd2f11c2782ec110bda92d92363ca49e8564
Wodnyan <gergoalbert.gta@gmail.com>
2020-05-14 11:50:10 +0300

    Minor changes

--- frontend/src/components/main/woof/AllWoofer.jsx
+++ frontend/src/components/main/woof/AllWoofer.jsx
@@ -2,16 +2,24 @@ import React, {useState, useEffect} from 'react'
 import Content from "./Content.jsx"
 import Load from "../load/Load.jsx"
 import axios from "axios"
+import socketIoClient from "socket.io-client"
 
 export default function AllWoofer(props){
   const [woof, setWoof] = useState([]);
   const abortController = new AbortController();
-  const url = "http://localhost:3000/api/woofer";
+  const URL = "http://localhost:3000/api/woofer";
+  const ENDPOINT = "http://localhost:3000/"
+  useEffect(() => {
+    const socket = socketIoClient(ENDPOINT);
+    socket.on("connect", () => {
+      socket.emit("Hello world")
+    })
+  })
   useEffect(()=>{
     const CancelToken = axios.CancelToken;
     const source = CancelToken.source();
     axios
-      .get(url, {
+      .get(URL, {
         cancelToken: source.token
       })
       .then((resp) => {
