3add2105cb7f72d649d5f7c4717c214e0a1807f2
Wodnyan <gergoalbert.gta@gmail.com>
2020-05-18 21:26:35 +0300

    Now the collapsibles height isn't hardcoded

--- frontend/src/components/main/expanding_card/ExpandingCard.jsx
+++ frontend/src/components/main/expanding_card/ExpandingCard.jsx
@@ -1,15 +1,23 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import style from "./ExpandingCard.scss"
 
 export default function ExpandingCard(props) {
   const {cardName} = props;
   const [visible, setVisible] = useState(false)
+  const ref = useRef(null);
+  const handleClick = () => {
+    setVisible(!visible)
+    const node = ref.current;
+    const nodeHeight = node.scrollHeight
+    node.style.height = visible ? "0px" : `${nodeHeight}px` 
+  }
+  //Add a Chevron
     return(
     <div className="expanding-card-container">
-      <div onClick={() => setVisible(!visible)}>
+      <div onClick={handleClick}>
         <h1 className="expanding-title">{cardName}</h1>
       </div>
-      <div className={"expanding-children" + (!visible ? "" : " expanding-children--visible")}>
+      <div ref={ref} className="expanding-children">
         {props.children}
       </div>
     </div>
