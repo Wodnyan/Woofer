a13a34c485260e4234f6bc2a964a791c7df7828c
Wodnyan <gergoalbert.gta@gmail.com>
2020-05-22 13:12:53 +0300

    Users can now delete their account altough their woofs still persist(not a feature)

--- frontend/src/components/main/settings/Settings.jsx
+++ frontend/src/components/main/settings/Settings.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Description from "./Description.jsx"
 import ExpandingCard from "../expanding_card/ExpandingCard.jsx"
 import SwitchSlider from "../switch/Switch.jsx"
-//TODO: Light mode
+import DeleteUserPrompt from "../delete_user_prompt/DeleteUserPrompt.jsx"
 function Settings(props){
   const {setDarkMode, darkMode} = props;
+  const [prompt, setPrompt] = useState(false);
   const handleClick = () => {
     setDarkMode(prevState => {
       localStorage.setItem("darkMode", !prevState)
       return !prevState
     });
   }
+  console.log(prompt)
   return (
     <div style={{marginTop: "80px"}}>
       <ExpandingCard cardName={"Description"}>
         <Description />
       </ExpandingCard>
       <ExpandingCard cardName={"Light Mode"}>
-        <h1 style={{color: "black"}}>Switch to light mode</h1>
+        <h1>Switch to light mode</h1>
         <SwitchSlider handleClick={handleClick} defaultValue={darkMode}/>
       </ExpandingCard>
       <ExpandingCard cardName={"Profile picture"}>
         <div>
-          <h1 style={{color: "#000"}}>Change your profile picture</h1>
+          <h1>Change your profile picture</h1>
         </div>
       </ExpandingCard>
+      <ExpandingCard cardName={"Delete Account"}>
+        <div>
+          <button onClick={() => setPrompt(true)}>Delete Account</button>
+        </div>
+      </ExpandingCard>
+      {prompt && <DeleteUserPrompt exit={() => setPrompt(false)} />}
     </div>
   )
 }
